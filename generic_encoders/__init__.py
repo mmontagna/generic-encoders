5a1621b992d9b08ea934413b00c22748f9970ba5
Marco Montagna <marcojoemontagna@gmail.com>
2018-05-30 09:27:47 -0700

    Add base64 encoder.

--- generic_encoders/__init__.py
+++ generic_encoders/__init__.py
@@ -1,4 +1,5 @@
 from .encoders.gzip_encoder import GzipEncoder
+from .encoders.base64_encoder import Base64Encoder
 
-
+base64 = Base64Encoder()
 gzip = GzipEncoder()
---
+++ generic_encoders/encoders/base64_encoder.py
@@ -0,0 +1,13 @@
+import base64
+import six
+from .encoder import Encoder
+
+class Base64Encoder(Encoder):
+  file_suffixes = ['b64', 'base64']
+  inputs = [six.binary_type]
+  outputs = six.binary_type
+  def __init__(self):
+    super(Base64Encoder, self)
+
+  encode = staticmethod(base64.b64encode)
+  decode = staticmethod(base64.b64decode)
