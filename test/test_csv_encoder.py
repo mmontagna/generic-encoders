205130363b66bcd7e15040420a4f3c0f7989c4aa
Marco Montagna <marco@mightysignal.com>
2018-08-10 14:59:58 -0700

    Add csv encoder test.

---
+++ test/test_csv_encoder.py
@@ -0,0 +1,26 @@
+import unittest
+import six
+
+from generic_encoders import csv
+
+class TestCsvEncoder(unittest.TestCase):
+
+    def test_csv_encoder_basic_dicts(self):
+        dicts = [{"a": "1"}, {"a": "2"}, {"a": "3"}]
+        self.assertEqual(
+          dicts,
+          csv.decode(
+            csv.encode(dicts)))
+
+    def test_csv_encoder_numbers_are_decoded_as_strings(self):
+        dicts = [{"a":  1}, {"a": 2.2}]
+        self.assertEqual(
+          [{"a":  "1"}, {"a": "2.2"}],
+          csv.decode(
+            csv.encode(dicts)))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
